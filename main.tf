52a7c7d20141baf328cf03c8397103fa194f36b8
Thomas Schrenk <36664716+tschrenk-corp@users.noreply.github.com>
2022-11-11 16:32:34 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -56,7 +56,7 @@ resource "ibm_is_subnet" "subnet3" {
   vpc             = "${ibm_is_vpc.vpc1.id}"
   zone            = "${var.zone3}"
   ipv4_cidr_block = "${var.zone3_cidr}"
-  depends_on      = ["ibm_is_vpc_address_prefix.vpc-ap3"]
+  depends_on      = [ibm_is_vpc_address_prefix.vpc-ap3]
 }
 
 resource "ibm_is_instance" "instance1" {
