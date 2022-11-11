a2eb92a5ada514c22572aee8f5173fd1258edf14
Thomas Schrenk <36664716+tschrenk-corp@users.noreply.github.com>
2022-11-11 18:30:34 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -131,10 +131,10 @@ resource "ibm_is_security_group_rule" "sg1_tcp_rule_80" {
   depends_on = [ibm_is_floating_ip.floatingip3]
 }
 
-resource "ibm_is_lb_pool_member" "lb1-pool-member3" {
-  count = 1
-  lb = "${ibm_is_lb.lb1.id}"
-  pool = "${ibm_is_lb_pool.lb1-pool.id}"
-  port = "80"
-  target_address = "${ibm_is_instance.instance3.primary_network_interface.0.primary_ipv4_address}"
+// resource "ibm_is_lb_pool_member" "lb1-pool-member3" {
+//  count = 1
+//  lb = "${ibm_is_lb.lb1.id}"
+//  pool = "${ibm_is_lb_pool.lb1-pool.id}"
+//  port = "80"
+//  target_address = "${ibm_is_instance.instance3.primary_network_interface.0.primary_ipv4_address}"
 }
